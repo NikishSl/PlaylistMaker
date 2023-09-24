e3733dfefaa64bc5c276d78172de78909e760543
NikishSL <slepchyoni@yandex.ru>
2023-09-24 15:46:01 +0700

    Спринт №9

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,75 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchBackButton = findViewById<ImageButton>(R.id.search_back_button)
+        val inputSearchText = findViewById<EditText>(R.id.input_search_text)
+        val searchClearButton = findViewById<ImageView>(R.id.clear_icon)
+
+        searchClearButton.setOnClickListener {
+            inputSearchText.setText("")
+            hideKeyboard()
+        }
+
+        searchBackButton.setOnClickListener {
+            finish()
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchClearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+        inputSearchText.addTextChangedListener(simpleTextWatcher)
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val inputSearchText = findViewById<EditText>(R.id.input_search_text)
+        val searchText = inputSearchText.text.toString()
+        outState.putString("searchText", searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val inputSearchText = findViewById<EditText>(R.id.input_search_text)
+        val searchText = savedInstanceState.getString("searchText", "")
+        inputSearchText.setText(searchText)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun hideKeyboard() {
+        val hideK = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        hideK.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+    }
+
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +12,59 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val settingsBackButton = findViewById<ImageButton>(R.id.settings_back_button)
+        val sharedButton = findViewById<ImageButton>(R.id.shared_button)
+        val supprotButton = findViewById<ImageButton>(R.id.support_button)
+        val privacyButton = findViewById<ImageButton>(R.id.privacy_button)
+
+        privacyButton.setOnClickListener {
+            privacyPolicy()
+        }
+
+        supprotButton.setOnClickListener {
+            contactSupport()
+        }
+
+        sharedButton.setOnClickListener {
+            shareApp()
+        }
 
         settingsBackButton.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun shareApp(){
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/profile/android-developer")
+
+        startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"))
+    }
+
+    private fun contactSupport() {
+        val email = "email"
+        val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+        val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:$email")
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+
+        try {
+            startActivity(Intent.createChooser(intent, "Выберите почтовый клиент"))
+            } catch (e: Exception) {
+        }
+    }
+
+    private fun privacyPolicy() {
+        val url = "https://yandex.ru/legal/practicum_offer"
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+
+        try {
+            startActivity(intent)
+            } catch (e: Exception) {
         }
     }
 }
